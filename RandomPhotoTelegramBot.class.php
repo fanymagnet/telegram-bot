1325e47dc7b58cb34f4d6482ffd22764f11c2467
fanymagnet <fanymagnet@mail.ru>
2018-04-28 20:15:55 +0300

    fix rand photo

--- RandomPhotoTelegramBot.class.php
+++ RandomPhotoTelegramBot.class.php
@@ -6,7 +6,7 @@
             $request = array(
                 'chat_id' => $chatId,
                 // Для того чтобы фото быо разное надо прикручивать к ссылке случайный хэш. Иначе фото шлется одно и то же.
-                'photo' => 'https://source.unsplash.com/random?hash=' . sha1(uniqid(microtime(TRUE), TRUE))
+                'photo' => 'https://source.unsplash.com/random?uniqid=' . sha1(uniqid(microtime(TRUE), TRUE))
             );
 
             $request = json_encode($request);
