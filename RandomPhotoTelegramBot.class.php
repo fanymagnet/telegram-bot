d8a5997fc6459a9caea3018434f76593dc3f22d4
fanymagnet <fanymagnet@mail.ru>
2018-05-03 12:13:38 +0300

    min fix photo

--- RandomPhotoTelegramBot.class.php
+++ RandomPhotoTelegramBot.class.php
@@ -6,7 +6,7 @@
             $request = array(
                 'chat_id' => $chatId,
                 // Для того чтобы фото было разное надо прикручивать к ссылке случайный хэш. Иначе фото шлется одно и то же.
-                'photo' => 'http://thecatapi.com/api/images/get?api_key=MzA3NzE0&random_hash=' . sha1(uniqid(microtime(TRUE), TRUE))
+                'photo' => 'http://thecatapi.com/api/images/get?api_key=MzA3NzE0&type=jpg,png&random_hash=' . sha1(uniqid(microtime(TRUE), TRUE))
             );
 
             $request = json_encode($request);
