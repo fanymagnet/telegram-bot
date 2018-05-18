9d7cf37cf6c4658129471e9e678476adfbc945f9
fanymagnet <fanymagnet@mail.ru>
2018-05-18 10:35:31 +0300

    fix index file

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
     require(__DIR__ . '/TelegramBot.class.php');
-    require(__DIR__ . '/GitLabTelegramBot.class.php');
-    require(__DIR__ . '/RandomPhotoTelegramBot.class.php');
-    require(__DIR__ . '/RandomAnekdotTelegramBot.class.php');
 
     $dataMessage = file_get_contents('php://input');
 
@@ -16,23 +13,35 @@
         break;
 
         case 'GitLab':
+
+            require(__DIR__ . '/GitLabTelegramBot.class.php');
+
             (new GitLabTelegramBot(@$_SERVER['HTTP_X_GITLAB_TOKEN']))
                 ->createMessage($dataMessage, @$_SERVER['HTTP_X_GITLAB_EVENT'])
                 ->createRequest(@$_GET['chatId'])
                 ->sendMessage();
+
         break;
 
         case 'RandomPhoto':
+
+            require(__DIR__ . '/RandomPhotoTelegramBot.class.php');
+
             (new RandomPhotoTelegramBot(@$_GET['secretToken']))
                 ->createRequest(@$_GET['chatId'])
                 ->sendPhoto();
+
         break;
 
         case 'RandomAnekdot':
+
+            require(__DIR__ . '/RandomAnekdotTelegramBot.class.php');
+
             (new RandomAnekdotTelegramBot(@$_GET['secretToken']))
                 ->createMessage()
                 ->createRequest(@$_GET['chatId'])
                 ->sendMessage();
+            
         break;
 
         default:
